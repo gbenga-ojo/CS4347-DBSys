c839cdfbe9b7512447632e11cbc734d0af4edda3
gbenga-ojo <149461744+gbenga-ojo@users.noreply.github.com>
2023-11-27 15:54:27 -0600

    Initial commit w/ updated latest Database.sql
    These are the files from MS Teams obtained Sun, 11.26.23

---
+++ Database Project - Queries.sql
@@ -0,0 +1,51 @@
+use company;
+
+-- QUERY #1
+select interviewers_name, interviewers_ID
+from interviewers, interviewees
+where interviewees = "Hellen Cole" and interviewees.J_ID = "11111";
+
+-- QUERY #2
+select J_ID
+from positions as p, department as d
+where d.D_Name = "Marketing" and d.D_ID = p.D_ID and p.date_posted like "2011-01-%";
+
+-- QUERY #3
+select FName, Lname, E_ID
+from employee as e, people as p
+where e.S_ID = null and e.E_ID = p.ID
+
+-- QUERY #4
+
+
+-- QUERY #5
+
+
+-- QUERY #6
+
+
+-- QUERY #7
+
+
+-- QUERY #8
+
+
+-- QUERY #9
+
+
+-- QUERY #10
+
+
+-- QUERY #11
+
+
+-- QUERY #12
+
+
+-- QUERY #13
+
+
+-- QUERY #14
+
+
+-- QUERY #15
---
+++ Database Project - Views.sql
@@ -0,0 +1,29 @@
+use company;
+
+-- VIEW #1
+create view avg_salary
+as select e_id, avg(amount)
+from pay
+group by e_id;
+
+-- VIEW #2
+create view interviews_passed
+as select Can_ID, num_passed
+from grades;
+
+-- VIEW #3
+create view num_product_sold
+as select Pr_ID, count(Pr_ID)
+from sale_history
+group by pr_id;
+
+-- VIEW #4
+create view product_price
+as select used_in, price * amount as price
+from parts_use, parts
+where price in (
+	select price
+    from parts as p, parts_use as u
+    where p.Pa_ID = u.Pa_ID
+)
+group by used_in;
